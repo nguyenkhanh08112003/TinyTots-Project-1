61536e7e1f3cfb8e5ccddbb562ad0d23008e9dbe
nguyenkhanh08112003 <91303308+nguyenkhanh08112003@users.noreply.github.com>
2022-05-31 09:08:27 +0700

    openweathermap

---
+++ xml/weathertest/js/weather.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var weatherApp = angular.module('weatherApp', []);
+
+weatherApp.controller('WeatherCtrl', function ($scope, $http){
+    $http.get("http://api.openweathermap.org/data/2.5/weather?lat=21.022736&lon=105.834560&appid=c5fa5112984ef1d54f8eb90a645bba36&units=metric")
+        .success(function (data){
+        if (data){
+
+            $scope.current = data.main.temp;
+            $scope.temp_min = data.main.temp_min;
+            $scope.temp_max = data.main.temp_max;
+            $scope.wind_speed = data.main.wind_speed;
+            $scope.clouds = data.clouds ? data.clouds.all : undefined;
+
+            var baseUrl = 'https://ssl.gstatic.com/onebox/weather/128/';
+            if ($scope.clouds<20){
+                $scope.img_url = baseUrl + 'sunny.png';
+            } else if($scope.clouds < 90){
+                $scope.img_url = baseUrl + 'partly_cloudy.png';
+            } else{
+                $scope.img_url = baseUrl + 'cloudy.png';
+            }
+        }
+    })
+        .error(function (data, status){
+            console.log(data);
+        })
+})
